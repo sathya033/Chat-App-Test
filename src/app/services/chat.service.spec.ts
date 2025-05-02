19e4de12601d0f8046fff66c798811d487a62c5a
sathiyamoorthim <sathiyamoorthimadhesh@gmail.com>
2025-05-02 12:59:50 +0530

    Remove unused imports in chat service tests

--- src/app/services/chat.service.spec.ts
+++ src/app/services/chat.service.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ChatService } from './chat.service';
-import { of, throwError } from 'rxjs';
 import { io } from 'socket.io-client';
 
 jest.mock('socket.io-client');
